11c7a4ae38a3d4bbff21316c77a4a2bf39e06e41
Maisam Afshar <maisam.afshar@gmail.com>
2022-06-13 10:07:15 +0430

    made standard text bold

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ allPostsData }) {
         <p>
         <strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry.
         </p>
-        <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        <p> Lorem Ipsum has been the industry's <strong>standard</strong> dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
         <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
         <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
